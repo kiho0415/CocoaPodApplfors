b4eba11ae4f450de4628146bc8a93d37a0a060b5
KIho Mmorita <kiho.h1.chukecheese@gmail.com>
2020-09-11 19:01:38 +0900

    done

--- CocoaPodApplfors/ViewController.swift
+++ CocoaPodApplfors/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PKHUD
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    override func viewDidAppear(_ animated: Bool){
+        super.viewDidAppear(animated)
+        HUD.flash(.success, delay: 2.0)//２秒後に表示
+    }
 
 }
 
